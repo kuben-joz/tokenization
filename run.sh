bb27e3b8618a70c439fc8a241bf6e4f2b6980d98
Jakub Jozefowicz <kuben.joz@gmail.com>
2022-12-08 22:15:08 +0100

    changged script

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-mkdir data
 mkdir data/conv
 g++ -Ofast -o generator generator-multithreaded.cpp
 ./generator -I data/GRCh38_full_analysis_set_plus_decoy_hla.fa -O data/conv
-python -m venv env
-source env/bin/activate
 pip install sentencepiece
 python tokenization.py
 
