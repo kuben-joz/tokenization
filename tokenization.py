11c006f1755c6361513db81fb03a3cbca8f1cdc5
Jakub Jozefowicz <kuben.joz@gmail.com>
2023-01-15 20:10:58 +0100

    back to tokenization with google implemnetation 1

---
+++ tokenization.py
@@ -0,0 +1,19 @@
+"""
+https://github.com/google/sentencepiece/issues/306
+            --unk_piece=[UNK]
+            --pad_piece=[PAD]
+            --user_defined_symbols=[CLS],[SEP],[MASK]
+            for bert with MLM and NSP
+"""
+
+import sentencepiece as spm
+import glob
+
+files = ','.join(glob.glob('/mnt/evafs/sfglab/data/chiabert-tokenizer/data/conv/chr*'))
+
+spm.SentencePieceTrainer.Train(input=files, model_prefix='chiabert', vocab_size=32000,
+                               user_defined_symbols=['[CLS]', '[SEP]', '[MASK]'],
+                               unk_piece='[UNK]', pad_piece='[PAD]',
+                               character_coverage=1.0, model_type='bpe',
+                               num_threads=48)
+
